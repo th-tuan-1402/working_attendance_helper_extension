bb3981387bffee3d7db8e4cf1d23c07e4da1cffb
[Lampart] Tran Thanh Tuan <tran_thanh_tuan@lampart-vn.com>
2024-03-22 14:37:51 +0700

    Add background task to check if it's should do check in/out

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -12,4 +12,35 @@ export default defineBackground(() => {
   chrome.notifications.onClicked.addListener(function(notificationId) {
     chrome.tabs.create({url: notificationId});
   });
+
+  // Set up an alarm to periodically wake up the background script
+  chrome.alarms.create("periodicAlarm", { periodInMinutes: 1 }); // Adjust period as needed
+
+  // Add an event listener to handle the alarm
+  chrome.alarms.onAlarm.addListener((alarm) => {
+      if (alarm.name === "periodicAlarm") {
+          // Perform tasks here to keep the background script active
+          console.log("Background script is active due to periodic alarm.");
+          sendMessageToContentScript({action: "checkTime"});
+      }
+  });
+
+  /**
+   * Send a message to any available tab
+   *
+   * @param {Object} message Message to send
+   */
+  function sendMessageToContentScript(message: object) {
+    // Query for any available tabs
+    chrome.tabs.query({ url: ['http://*/*', 'https://*/*'] }, function (tabs) {
+      // If there are any tabs found
+      if (tabs && tabs.length > 0) {
+        // Send the message to the first tab
+        chrome.tabs.sendMessage(tabs[0].id, message);
+      } else {
+        // If no tabs were found, log an error
+        console.error("No tabs found.");
+      }
+    });
+  }
 });
